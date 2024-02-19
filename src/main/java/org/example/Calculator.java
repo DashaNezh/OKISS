b9291821128ca35b02b63655fc16e4a22084f3b6
DashaNezh <melkomukova.dara@mail.ru>
2024-02-19 10:52:13 +0500

    1

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,4 +1,40 @@
 package org.example;
-
+import java.util.Scanner;
 public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите первое число: ");
+        double num1 = in.nextDouble();
+        System.out.println("Введите второе число: ");
+        double num2 = in.nextDouble();
+        System.out.println("Введите операцию (+, -, *, /): ");
+        char operation = in.next().charAt(0);
+
+        double result = 0;
+
+        switch (operation){
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1*num2;
+                break;
+            case '/':
+                if (num2 != 0){
+                    result = num1 / num2;
+                }
+                else{
+                    System.out.println("Деление на ноль!");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Неверная операция!");
+                return;
+        }
+        System.out.println("Результат: " + result);
+    }
 }
