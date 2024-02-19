bd38112a594c12ff071ef0551917ee4455b7754f
DashaNezh <melkomukova.dara@mail.ru>
2024-02-19 15:22:27 +0500

    1

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,40 +1,60 @@
 package org.example;
+
 import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите первое число: ");
-        double num1 = in.nextDouble();
-        System.out.println("Введите второе число: ");
-        double num2 = in.nextDouble();
-        System.out.println("Введите операцию (+, -, *, /): ");
-        char operation = in.next().charAt(0);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите первое число:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Введите второе число:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Введите операцию (+, -, *, /):");
+        char operation = scanner.next().charAt(0);
 
         double result = 0;
 
-        switch (operation){
+        switch (operation) {
             case '+':
-                result = num1 + num2;
+                result = addition(num1, num2);
                 break;
             case '-':
-                result = num1 - num2;
+                result = subtraction(num1, num2);
                 break;
             case '*':
-                result = num1*num2;
+                result = multiplication(num1, num2);
                 break;
             case '/':
-                if (num2 != 0){
-                    result = num1 / num2;
-                }
-                else{
-                    System.out.println("Деление на ноль!");
-                    return;
-                }
+                result = division(num1, num2);
                 break;
             default:
                 System.out.println("Неверная операция!");
-                return;
         }
+
         System.out.println("Результат: " + result);
+    }
+
+    public static double addition(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtraction(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double multiplication(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double division(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("Деление на ноль!");
+            return 0;
+        } else {
+            return num1 / num2;
+        }
     }
 }
--- src/test/java/org/example/TestCalculator.java
+++ src/test/java/org/example/TestCalculator.java
@@ -1,4 +1,20 @@
 package org.example;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class TestCalculator {
+    @Test
+    void testAddition(){
+        //Arange - создание текстового окружения
+        final double NUMBER1 = 2;
+        final double NUMBER2 = 3;
+        double expectedResult = 5;
+        double actualResult;
+        final double DELTA = 1e-6;
+        //Act - действия (исполнения)
+        actualResult = Calculator.addition(NUMBER1, NUMBER2);
+        //Assert - сравнение фактического и ожидаемого результатов
+        Assert.assertEquals(actualResult, expectedResult, DELTA);
+    }
 }
